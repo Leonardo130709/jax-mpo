Embedding dim added to encoder, actor remains on cpu, mixed precision is somewhat fixed, egl rendering flag added to dmc envs.

src/actor.py
@@ -26,7 +26,7 @@ class Actor:
                  client: reverb.Client
                  ):
 
-        @partial(jax.jit, backend="cpu", static_argnums=(3,))
+        @partial(jax.jit, backend="cpu")
         @chex.assert_max_traces(n=2)
         def _act(params: hk.Params,
                  key: jax.random.PRNGKey,
@@ -37,10 +37,11 @@ class Actor:
             state = networks.encoder(params, observation)
             policy_params = networks.actor(params, state)
             dist = networks.make_policy(*policy_params)
-            if training:
-                action = dist.sample(seed=key)
-            else:
-                action = dist.distribution.mean()
+            action = jax.lax.select(
+                training,
+                dist.sample(seed=key),
+                dist.distribution.mean()
+            )
             action = jnp.clip(action, a_min=-1, a_max=1)
             return action
 
@@ -141,6 +142,11 @@ def _get_reverb_metrics(client: reverb.Client) -> dict[str, float]:
         "sampling_ms",
         "inserting_ms",
         "sleeping_ms",
-        "waiting_for_inserts_ms"
+        "waiting_for_sampling_ms",
+        "waiting_for_inserts_ms",
     )
-    return {f"reverb_{key}": getattr(info, key) for key in stats}
+    reverb_info = {}
+    for key in stats:
+        if hasattr(info, key):
+            reverb_info[f"reverb_{key}"] = getattr(info, key)
+    return reverb_info

src/learner.py
@@ -62,8 +62,7 @@ class MPOLearner:
             log_alpha_mean=jnp.full(act_dim, cfg.init_log_alpha_mean),
             log_alpha_std=jnp.full(act_dim, cfg.init_log_alpha_std)
         )
-        # to param or to compute?
-        _dual_params = prec.cast_to_compute(_dual_params)
+        _dual_params = prec.cast_to_output(_dual_params)
 
         adam = functools.partial(optax.adam,
                                  b1=cfg.adam_b1,
@@ -84,6 +83,11 @@ class MPOLearner:
             )
         else:
             _loss_scale = jmp.NoOpLossScale()
+
+        param_groups = networks.split_params(_params)
+        names = ("Actor", "Encoder", "Critic")
+        for pg, name in zip(param_groups, names):
+            print(f"{name} params: {hk.data_structures.tree_size(pg)}")
 
         self._state = MPOState(
             params=_params,
@@ -130,12 +134,15 @@ class MPOLearner:
             target_s_t = jnp.repeat(target_s_t[jnp.newaxis],
                                     cfg.num_actions, axis=0)
 
+            # z_t.shape : (num_actions, num_actor_quantiles, num_critic_heads)
             z_t = networks.critic(target_params, target_s_t, a_t, tau_t)
-            z_t = jnp.min(z_t, axis=2)
+            z_t = jnp.min(z_t, axis=2)  # pessimistic ensemble
+            # z_tm1.shape: (num_critic_quantiles, num_critic_heads)
             z_tm1 = networks.critic(params, s_tm1, a_tm1, tau_tm1)
             v_t = jnp.mean(z_t, axis=0)
             q_t = jnp.mean(z_t, axis=1)  # risk-neutral agent
             target_z_tm1 = sg(r_t + discount_t * v_t)
+
             critic_loss_fn = jax.vmap(
                 ops.quantile_regression_loss,
                 in_axes=(1, None, None, None)
@@ -173,7 +180,6 @@ class MPOLearner:
                 kl_loss, dual_loss = ops.scaled_and_dual_loss(
                     kl, duals, epsilon, per_dimension=True
                 )
-                chex.assert_rank([ce_loss, kl_loss, dual_loss], 0)
                 loss = ce_loss + kl_loss + dual_loss
                 pfx = lambda k: f"{prefix}_{k}"
                 met = {
@@ -188,7 +194,6 @@ class MPOLearner:
                 fixed_std, alpha_mean, cfg.epsilon_mean, prefix="mean")
             std_loss, metrics_std = policy_loss(
                 fixed_mean, alpha_std, cfg.epsilon_std, prefix="std")
-            chex.assert_rank([critic_loss, mean_loss, std_loss, temperature_loss], 0)
             total_loss = critic_loss + mean_loss + std_loss + temperature_loss
 
             metrics = dict(
@@ -216,41 +221,43 @@ class MPOLearner:
             optim_state = mpostate.optim_state
             dual_optim_state = mpostate.dual_optim_state
             step = mpostate.step
+
             params_grads, dual_grads = grads
 
             params_update, optim_state = optim.update(
-                params_grads, optim_state)
+                params_grads, optim_state
+            )
             dual_update, dual_optim_state = dual_optim.update(
-                dual_grads, dual_optim_state)
+                dual_grads, dual_optim_state
+            )
             params = optax.apply_updates(params, params_update)
             dual_params = optax.apply_updates(dual_params, dual_update)
 
-            actor_params, encoder_params, critic_params =\
-                networks.split_params(params)
-            target_actor_params, target_encoder_params, target_critic_params = \
-                networks.split_params(target_params)
+            target_params = optax.periodic_update(
+                params, target_params, step, cfg.targets_update_period)
 
-            target_actor_params = optax.periodic_update(
-                actor_params,
-                target_actor_params,
-                step,
-                cfg.target_actor_update_period
-            )
-            (target_encoder_params, target_critic_params) =\
-                optax.periodic_update(
-                    (encoder_params, critic_params),
-                    (target_encoder_params, target_critic_params),
-                    step,
-                    cfg.target_critic_update_period
-                )
-            target_params.update(target_actor_params)
-            target_params.update(target_encoder_params)
-            target_params.update(target_critic_params)
+            # actor_params, encoder_params, critic_params =\
+            #     networks.split_params(params)
+            # target_actor_params, target_encoder_params, target_critic_params = \
+            #     networks.split_params(target_params)
+            #
+            # target_actor_params = optax.periodic_update(
+            #     actor_params,
+            #     target_actor_params,
+            #     step,
+            #     cfg.target_actor_update_period
+            # )
+            # (target_encoder_params, target_critic_params) =\
+            #     optax.periodic_update(
+            #         (encoder_params, critic_params),
+            #         (target_encoder_params, target_critic_params),
+            #         step,
+            #         cfg.target_critic_update_period
+            #     )
+            # target_params.update(target_actor_params)
+            # target_params.update(target_encoder_params)
+            # target_params.update(target_critic_params)
 
-            metrics = dict(
-                params_grad_norm=optax.global_norm(params_grads),
-                dual_grad_norm=optax.global_norm(dual_grads),
-            )
             return mpostate._replace(
                 params=params,
                 target_params=target_params,
@@ -258,7 +265,7 @@ class MPOLearner:
                 optim_state=optim_state,
                 dual_optim_state=dual_optim_state,
                 step=step+1
-            ), metrics
+            )
 
         @chex.assert_max_traces(n=1)
         def _step(mpostate: MPOState, data):
@@ -271,30 +278,24 @@ class MPOLearner:
             observations, actions, rewards, discounts, next_observations =\
                 map(
                     data.get,
-                    ("observations",
-                     "actions",
-                     "rewards",
-                     "discounts",
-                     "next_observations")
+                    ("observations", "actions", "rewards",
+                     "discounts", "next_observations")
                 )
             chex.assert_tree_shape_prefix(actions, (cfg.batch_size,))
             observations = networks.preprocess(observations)
             next_observations = networks.preprocess(next_observations)
+            actions, rewards, discounts =\
+                prec.cast_to_compute((actions, rewards, discounts))
+
             keys = jax.random.split(rng_key, num=cfg.batch_size+1)
             rng_key, subkeys = keys[0], keys[1:]
 
             in_axes = 4 * (None,) + 6 * (0,)
-            args = (
+            grad_fn = jax.grad(mpo_loss, argnums=(0, 1), has_aux=True)
+            grads, metrics = jax.vmap(grad_fn, in_axes=in_axes)(
                 params, dual_params, target_params, loss_scale, subkeys,
                 observations, actions, rewards, discounts, next_observations
             )
-            in_axes = tuple(
-                jax.tree_util.tree_map(lambda t: axis, val)
-                for axis, val in zip(in_axes, args)
-            )
-
-            grad_fn = jax.grad(mpo_loss, argnums=(0, 1), has_aux=True)
-            grads, metrics = jax.vmap(grad_fn, in_axes=in_axes)(*args)
             grads, metrics = jax.tree_util.tree_map(
                 lambda t: jnp.mean(t, axis=0),
                 (grads, metrics)
@@ -303,18 +304,16 @@ class MPOLearner:
             grads_finite = jmp.all_finite(grads)
             loss_scale = loss_scale.adjust(grads_finite)
 
-            met_placeholder = {
-                "params_grad_norm": float("inf"),
-                "dual_grad_norm": float("inf"),
-            }
-            mpostate, met = jmp.select_tree(
+            mpostate = jmp.select_tree(
                 grads_finite,
                 update_step(mpostate, grads),
-                (mpostate, met_placeholder)
+                mpostate
             )
             metrics.update(loss_scale=loss_scale.loss_scale,
                            grads_finite=grads_finite,
-                           **met)
+                           params_grad_norm=optax.global_norm(grads[0]),
+                           dual_grad_norm=optax.global_norm(grads[1]),
+                           )
 
             return mpostate._replace(
                 rng_key=rng_key,
@@ -323,11 +322,6 @@ class MPOLearner:
 
         self._step = jax.jit(_step)
 
-        param_groups = networks.split_params(_params)
-        names = ("Actor", "Encoder", "Critic")
-        for pg, name in zip(param_groups, names):
-            print(f"{name} params: {hk.data_structures.tree_size(pg)}")
-
     def run(self):
         logger = TFSummaryLogger(logdir=self._cfg.logdir,
                                  label="train",
@@ -335,7 +329,6 @@ class MPOLearner:
         while True:
             data = next(self._data_iterator)
             info, data = data
-            # data = jax.device_put(data)
             self._state, metrics = self._step(self._state, data)
 
             step = self._state.step

src/main.py
@@ -1,9 +1,9 @@
 import os
-import multiprocessing as mp
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 
+import multiprocessing as mp
 import tensorflow as tf
 tf.config.set_visible_devices([], "GPU")
 
@@ -40,7 +40,7 @@ def main():
     os.makedirs(config.logdir)
     config.save(config.logdir + '/config.yaml')
     builder = Builder(config)
-    chex.disable_asserts()
+    # chex.disable_asserts()
     env, env_specs = builder.make_env()
     server_address = f"localhost:{config.reverb_port}"
     server = mp.Process(target=run_server,

src/networks.py
@@ -160,7 +160,7 @@ class DistributionalCritic(hk.Module):
         x = jnp.concatenate([state, action], -1)
         tau = QuantileNetwork(x.shape[-1], self.quantile_embedding_dim)(tau)
         x = jnp.expand_dims(x, -2)
-        # warn: implicit broadcasting
+        # warn: implicit broadcast.
         return MLP(self.layers + (1,), self.act, self.norm)(x * tau)
 
 
@@ -183,6 +183,7 @@ class CriticsEnsemble(hk.Module):
 class Encoder(hk.Module):
     def __init__(self,
                  keys: str,
+                 emb_dim: int,
                  mlp_layers: Iterable[int],
                  pn_layers: Iterable[int],
                  cnn_kernels: Iterable[int],
@@ -194,6 +195,7 @@ class Encoder(hk.Module):
                  ):
         super().__init__(name=name)
         self.keys = keys
+        self.emb_dim = emb_dim
         self.mlp_layers = tuple(mlp_layers)
         self.pn_layers = tuple(pn_layers)
         self.cnn_kernels = tuple(cnn_kernels)
@@ -229,13 +231,20 @@ class Encoder(hk.Module):
                 outputs.append(self._mlp(mlp_features))
 
         if pn_features is not None:
+            # TODO: assert there is only one pcd in input.
             outputs.append(self._pn(pn_features))
         if cnn_features is not None:
             outputs.append(self._cnn(cnn_features))
         if not outputs:
             raise ValueError(f"No valid {self.keys!r} in {obs.keys()}")
 
-        return jnp.concatenate(outputs, -1)
+        features = jnp.concatenate(outputs, -1)
+        emb = MLP((self.emb_dim,),
+                  act=self.act,
+                  norm=self.norm,
+                  activate_final=True)
+
+        return emb(features)
 
     def _cnn(self, x):
         for kernel in self.cnn_kernels:
@@ -262,7 +271,9 @@ class Encoder(hk.Module):
         return jnp.max(x, -2)
 
 
-def _partition(items: Dict[str, jnp.ndarray], n: int = 3) -> list[jnp.ndarray]:
+def _partition(items: Dict[str, jnp.ndarray],
+               n: int = 3
+               ) -> list[jnp.ndarray | None]:
     """Splits inputs in groups by number of dimensions."""
     structures = list([] for _ in range(n))
     for key, value in items.items():
@@ -298,7 +309,8 @@ def make_networks(cfg: MPOConfig,
     hk.mixed_precision.set_policy(CriticsEnsemble, prec)
 
     dummy_obs = jax.tree_util.tree_map(
-        lambda sp: sp.generate_value(), observation_spec
+        lambda sp: sp.generate_value(), observation_spec,
+        is_leaf=lambda x: isinstance(x, specs.Array)
     )
 
     def preprocess(data: dict[str, jnp.ndarray]):
@@ -324,6 +336,7 @@ def make_networks(cfg: MPOConfig,
         )
         encoder = Encoder(
             cfg.keys,
+            cfg.encoder_emb_dim,
             cfg.mlp_layers,
             cfg.pn_layers,
             cfg.cnn_kernels,

src/utils/env_loop.py
@@ -6,20 +6,20 @@ import numpy as np
 import reverb
 from jax.tree_util import tree_map
 
-Array = np.ndarray
+Action = Array = np.ndarray
 Observation = Dict[str, Array]
 
 
 class Trajectory(TypedDict, total=False):
     observations: list[Observation]
-    actions: list[Array]
+    actions: list[Action]
     rewards: list[float]
     discounts: list[float]
     next_observations: list[Observation]
 
 
 def environment_loop(env: dm_env.Environment,
-                     policy: Callable[[Observation], Array],
+                     policy: Callable[[Observation], Action],
                      prev_timestep: dm_env.TimeStep,
                      max_timesteps: int = float("inf"),
                      ) -> Tuple[Trajectory, dm_env.TimeStep]:
@@ -113,21 +113,19 @@ class Adder:
 
         with self._client.trajectory_writer(num_keep_alive_refs=1) as writer:
             for i in range(tr_length):
-                writer.append(tree_slice(
-                    i, trajectory,
-                    is_leaf=lambda x: isinstance(x, list)
-                ))
-
-                if i < 1:
-                    continue
-
+                writer.append(
+                    tree_slice(
+                        i, trajectory,
+                        is_leaf=lambda x: isinstance(x, list)
+                    )
+                )
                 # o_tm1, a_tm1, r_t, d_t, o_t
                 writer.create_item(
                     table="replay_buffer",
                     priority=1.,
                     trajectory=tree_slice(-1, writer.history)
                 )
-                # writer.flush(block_until_num_items=10)
+                writer.flush(block_until_num_items=20)
 
 
 def tree_slice(sl, tree, is_leaf=None):

src/utils/ops.py
@@ -19,6 +19,7 @@ def scaled_and_dual_loss(loss: Array,
     """Lagrange multiplier loss."""
     chex.assert_rank(epsilon, 0)
     chex.assert_type([loss, duals], float)
+    chex.assert_is_broadcastable(loss.shape, duals.shape)
 
     sg = jax.lax.stop_gradient
     scaled_loss = sg(duals) * loss
@@ -36,7 +37,7 @@ def quantile_regression_loss(predictions: Array,
                              targets: Array,
                              hubber_delta: float
                              ) -> Array:
-
+    """Distributional critic loss."""
     chex.assert_type([predictions, pred_quantiles, targets], float)
     chex.assert_rank([predictions, pred_quantiles, targets], 1)
     chex.assert_equal_shape([predictions, pred_quantiles])
@@ -44,7 +45,7 @@ def quantile_regression_loss(predictions: Array,
 
     targets = sg(targets)
     resids = targets[jnp.newaxis, :] - predictions[:, jnp.newaxis]
-    ind = (resids < 0.).astype(pred_quantiles.dtype)
+    ind = (resids < 0).astype(pred_quantiles.dtype)
     weight = jnp.abs(pred_quantiles[:, jnp.newaxis] - ind)
     if hubber_delta > 0:
         loss = optax.huber_loss(resids, delta=hubber_delta) / hubber_delta
@@ -62,6 +63,7 @@ def cross_entropy_loss(dist: tfd.Distribution,
     chex.assert_type([actions, normalized_weights], float)
 
     log_probs = dist.log_prob(actions)
+
     chex.assert_rank([log_probs, normalized_weights], 1)
     chex.assert_equal_shape([log_probs, normalized_weights])
 
@@ -74,7 +76,7 @@ def temperature_loss_and_normalized_weights(
         epsilon: float,
         tv_constraint: float
 ) -> Tuple[Array, Array]:
-    """Direct dual constraint as a part of MPO loss."""
+    """Direct dual constraint as a part of CMPO loss."""
     chex.assert_type([temperature, q_values, epsilon, tv_constraint], float)
     chex.assert_rank(
         [temperature, q_values, epsilon, tv_constraint], [0, 1, 0, 0]
@@ -93,11 +95,10 @@ def temperature_loss_and_normalized_weights(
     tempered_q_values = sg(clipped) + straight_through
     tempered_q_values = tempered_q_values.astype(jnp.float32)
 
-    # tempered_q_values = sg(q_values) / temperature
     normalized_weights = jax.nn.softmax(tempered_q_values)
     normalized_weights = sg(normalized_weights)
 
-    log_num_actions = jnp.log(q_values.size).astype(q_values.dtype)
+    log_num_actions = jnp.log(q_values.size)
     q_logsumexp = logsumexp(tempered_q_values)
     temperature_loss = epsilon + q_logsumexp - log_num_actions
     temperature_loss *= temperature